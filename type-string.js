2cae6a9826b3649754aa5e8e2e41953016b4bfc4
Marcus Vinicius Arruda Carneiro <marcus@MacBook-Pro-de-Marcus-2.local>
2022-12-09 16:35:11 -0300

    exemplos da aula 1

---
+++ type-string.js
@@ -0,0 +1,17 @@
+const texto1 = 'Olá, mundo!';
+const texto2 = 'Olá, mundo!';
+const senha = 'SenhaSuperSegura456!';
+const StringDeNumeros = '34567';
+
+const citacao = 'Meu nome é ';
+const meuNome = 'Marcus';
+console.log(citacao + meuNome);
+
+// concatenação (+)
+
+
+
+//template string ou template literal
+
+console.log(`Literal é assim um em uma linha
+outro em outra linha... ${meuNome}`)
